cd0e92ae88b5240586092d0b9cf973945719ebc0
your namexiasiyu <673312386@qq.com>
2018-03-24 21:51:42 +0800

    备注

--- x/src/main/java/SerializeTest.java
+++ x/src/main/java/SerializeTest.java
@@ -1,2 +1,11 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class SerializeTest {
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        FileOutputStream fileOutputStream = new FileOutputStream("temp.out");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+    }
 }
